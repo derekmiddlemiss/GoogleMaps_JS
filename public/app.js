bab55dcc080290882ce8b3bf5c350f3f4d14cfe9
Derek Middlemiss <derekmiddlemiss@gmail.com>
2017-09-05 20:12:05 +0100

    Markers now report positions, button to go to AeroGraveyard in Tucson, switch map to satellite mode

---
+++ public/app.js
@@ -0,0 +1,13 @@
+var initialize = function(){
+  var center = { lat: 51.507351, lng: -0.127758 };
+  var mapDiv = document.getElementById( 'main-map' );
+  var mainMap = new MapWrapper( mapDiv, center, 10 );
+  mainMap.addMarker( center );
+  mainMap.addClickEvent();
+  var bounceButton = document.querySelector( '#button-bounce-markers' );
+  bounceButton.addEventListener( 'click', mainMap.bounceMarkers.bind( mainMap ) );
+  var aeroGraveyardButton = document.querySelector( '#button-aero-graveyard' );
+  aeroGraveyardButton.addEventListener( 'click', mainMap.centerAtAeroGraveyard.bind( mainMap ) );
+}
+
+window.addEventListener( 'load', initialize );
---
+++ public/mapWrapper.js
@@ -0,0 +1,44 @@
+var MapWrapper = function( container, coords, zoom ){
+  this.googleMap = new google.maps.Map( container, {
+    center: coords,
+    zoom: zoom
+  });
+  this.markers = [];
+}
+
+MapWrapper.prototype.addMarker = function( coords ) {
+  var marker = new google.maps.Marker({
+    position: coords,
+    map: this.googleMap
+  });
+  marker.info = new google.maps.InfoWindow({
+    content: "This is at " + coords.lat + ", " + coords.lng
+  })
+  google.maps.event.addListener( marker, 'click', function(){
+    marker.info.open( this.googleMap, marker );
+  })
+
+  this.markers.push( marker );
+},
+
+MapWrapper.prototype.addClickEvent = function(){
+  google.maps.event.addListener( this.googleMap, 'click', function(event){
+    var position = { lat: event.latLng.lat(), lng: event.latLng.lng() };
+    this.addMarker( position );
+  }.bind( this ) );
+},
+
+MapWrapper.prototype.bounceMarkers = function(){
+  this.markers.forEach( function( marker ){ 
+    marker.setAnimation( google.maps.Animation.BOUNCE );
+  });
+},
+
+MapWrapper.prototype.centerAtAeroGraveyard = function(){
+  var aeroGraveyardPosition = { lat: 32.152289 , lng: -110.837631 };
+  this.googleMap.setCenter( aeroGraveyardPosition );
+  this.googleMap.mapTypeId = google.maps.MapTypeId.SATELLITE
+  this.googleMap.setZoom( 16 );
+}
+
+
